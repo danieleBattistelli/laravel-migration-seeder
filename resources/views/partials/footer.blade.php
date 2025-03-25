90bc139705ac143946f6b62e86348dafe628b546
danieleBattistelli <bomber81@gmail.com>
2025-03-25 11:45:30 +0100

    first commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,11 @@
+
+<footer class="bg-dark text-white text-center py-3">
+    <div class="container">
+        <p>&copy; {{ date('Y') }} La tua azienda. Tutti i diritti riservati.</p>
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="#" class="text-white">Privacy</a></li>
+            <li class="list-inline-item"><a href="#" class="text-white">Termini</a></li>
+            <li class="list-inline-item"><a href="#" class="text-white">Contattaci</a></li>
+        </ul>
+    </div>
+</footer>
